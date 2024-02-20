3ec17dde7d4c61f60b5631930921834173143517
Juan Garcia <Juanmanu2350@gmail.com>
2024-02-20 00:52:03 -0600

    Agregar archivos de proyecto.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _dbContext = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _dbContext.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
